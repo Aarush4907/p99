0c8e2f336795b0d3474e8b1e4d5ba070a3f1b389
Aarush4907 <76614253+Aarush4907@users.noreply.github.com>
2021-01-12 16:21:52 +0530

    Add files via upload

---
+++ removeFiles.py
@@ -0,0 +1,16 @@
+import os
+import shutil
+import time
+deletedFiles = 0
+
+def main():
+    path = ""
+    days = 30
+    seconds = time.time()-(days*60*60*24)
+    if os.path.exists(path):
+        if path.exists:
+            deletedFiles = deletedFiles +1
+        else 
+            print("path does not exist")
+
+main()            
